9f989d3e24f2cb6ab6dbc151bc4807b498bc1e48
Oyegoke Tomisin <tomisin18@gmail.com>
2019-01-29 19:05:15 +0100

    Initial Commit

---
+++ ImageProcessing/RGBA.swift
@@ -0,0 +1,82 @@
+//
+//  RGBA.swift
+//  ImageProcessing
+//
+//  Created by OYEGOKE TOMISIN on 26/01/2019.
+//  Copyright © 2019 com.OyegokeTomisin. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+struct Pixel {
+    var value: UInt32
+    var red: UInt8 {
+        get { return UInt8(value & 0xFF) }
+        set { value = UInt32(newValue) | (value & 0xFFFFFF00) }
+    }
+    var green: UInt8 {
+        get { return UInt8((value >> 8) & 0xFF) }
+        set { value = (UInt32(newValue) << 8) | (value & 0xFFFF00FF) }
+    }
+    var blue: UInt8 {
+        get { return UInt8((value >> 16) & 0xFF) }
+        set { value = (UInt32(newValue) << 16) | (value & 0xFF00FFFF) }
+    }
+    var alpha: UInt8 {
+        get { return UInt8((value >> 24) & 0xFF) }
+        set { value = (UInt32(newValue) << 24) | (value & 0x00FFFFFF) }
+    }
+}
+
+public struct RGBA {
+    var pixels: UnsafeMutableBufferPointer<Pixel>
+    var width: Int
+    var height: Int
+    
+    init?(image: UIImage) {
+        guard let cgImage = image.cgImage else { return nil }
+        width = Int(image.size.width)
+        height = Int(image.size.height)
+        let bitsPerComponent = 8
+        let bytesPerPixel = 4
+        let bytesPerRow = width * bytesPerPixel
+        let imageData = UnsafeMutablePointer<Pixel>.allocate(capacity: width * height)
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue
+        bitmapInfo |= CGImageAlphaInfo.premultipliedLast.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
+        guard let imageContext = CGContext(data: imageData, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo) else { return nil }
+        imageContext.draw(cgImage, in: CGRect(origin: CGPoint(x: 0,y :0), size: image.size))
+        pixels = UnsafeMutableBufferPointer<Pixel>(start: imageData, count: width * height)
+    }
+    
+    public func toUIImage() -> UIImage? {
+        let bitsPerComponent = 8
+        let bytesPerPixel = 4
+        let bytesPerRow = width * bytesPerPixel
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue
+        bitmapInfo |= CGImageAlphaInfo.premultipliedLast.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
+        let imageContext = CGContext(data: pixels.baseAddress, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo, releaseCallback: nil, releaseInfo: nil)
+        guard let cgImage = imageContext!.makeImage() else {return nil}
+        let image = UIImage(cgImage: cgImage)
+        return image
+    }
+}
+
+public func processTask(with image: UIImage, and values:(r: Int, g: Int, b:Int)) -> RGBA {
+    let rgbImage = RGBA(image: image)!
+    
+    for index in 0 ..< rgbImage.pixels.count{
+        var pixel = rgbImage.pixels[index]
+        
+        pixel.red =  UInt8(min(255, values.r + Int(pixel.red)))
+        pixel.green = UInt8(min(255, values.g + Int(pixel.green)))
+        pixel.blue =  UInt8(min(255, values.b + Int(pixel.blue)))
+
+        rgbImage.pixels[index] = pixel
+    }
+   
+    return rgbImage
+}
--- ImageProcessing/ViewController.swift
+++ ImageProcessing/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var display: UIImageView!
+    @IBOutlet weak var RSlider: UISlider!
+    @IBOutlet weak var GSlider: UISlider!
+    @IBOutlet weak var BSlider: UISlider!
+    
+    var R: Int = 0
+    var G: Int = 0
+    var B: Int = 0
+    let img_name = "pokemon"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        display.image = UIImage(named: img_name)
     }
-
-
+    
+    @IBAction func reloadImageButtonTapped(_ sender: Any) {
+        display.image = UIImage(named: img_name)
+    }
+    
+    @IBAction func RSliderValueChanged(_ sender: UISlider) {
+        R = Int(sender.value)
+    }
+    
+    @IBAction func GSliderValueChanged(_ sender: UISlider) {
+        G = Int(sender.value)
+    }
+    
+    @IBAction func BSliderValueChanged(_ sender: UISlider) {
+        B = Int(sender.value)
+    }
+    
+    @IBAction func applyButtonTapped(_ sender: Any) {
+        processImage(with: (r: R, g: G, b: B))
+    }
+    
+    func processImage(with rgbValues: (r: Int, g: Int, b:Int)){
+        
+        if let image = UIImage(named: img_name){
+            DispatchQueue(label: "Processing image").async {
+                let newImage = processTask(with: image, and: rgbValues).toUIImage()
+                DispatchQueue.main.async {
+                    self.display.image = newImage
+                }
+            }
+        }
+    }
 }
 
